a1cba5da979e6da964f9c865a763792f3f02c684
Filip Procházka <filip@prochazka.su>
2017-02-25 11:13:19 +0100

    ExternalService better exceptions

--- src/Mock/ExternalService.php
+++ src/Mock/ExternalService.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace Workshop\Mock;
 
-use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Exception\ServerException;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
-use function GuzzleHttp\Psr7\stream_for;
 use Kdyby;
 use Psr\Http\Message\ResponseInterface;
+use function GuzzleHttp\Psr7\stream_for;
 
 class ExternalService
 {
@@ -32,10 +33,10 @@ class ExternalService
 			throw new ConnectException('Connection refused', $request);
 		}
 		if (mt_rand(0, 10) == 5) {
-			throw new BadResponseException('You fucked it up', $request, new Response(422));
+			throw new ClientException('You fucked it up', $request, new Response(422));
 		}
 		if (mt_rand(0, 20) == 5) {
-			throw new BadResponseException('Shit happens', $request, new Response(500));
+			throw new ServerException('Shit happens', $request, new Response(500));
 		}
 
 		return new Response(
